e15e155c3b1d0efd01e81ad8517ebfce8ac9517d
greksw <greksw@gmail.com>
2024-11-07 10:15:28 +0300

    Update pxe_install.sh
    Описание изменений:
    Ubuntu 24.04.1 LTS:
    
    1)Использована актуальная ссылка для ISO-образа Ubuntu 24.04.1:
    wget https://releases.ubuntu.com/24.04.1/ubuntu-24.04.1-desktop-amd64.iso -O /var/lib/tftpboot/ubuntu-installer/amd64/ubuntu-24.04.1-desktop-amd64.iso
    
    Извлечены необходимые файлы vmlinuz и initrd из образа:
    mount -o loop /var/lib/tftpboot/ubuntu-installer/amd64/ubuntu-24.04.1-desktop-amd64.iso /mnt
    cp /mnt/casper/vmlinuz /var/lib/tftpboot/ubuntu-installer/amd64/
    cp /mnt/casper/initrd /var/lib/tftpboot/ubuntu-installer/amd64/
    umount /mnt
    AlmaLinux 9.4:
    
    2)Использована актуальная ссылка для ISO-образа AlmaLinux 9.4:
    wget https://raw.repo.almalinux.org/almalinux/9.4/live/x86_64/AlmaLinux-9.4-x86_64-Live-GNOME-Mini.iso -O /var/lib/tftpboot/almalinux-9.4/isolinux/AlmaLinux-9.4-x86_64-Live-GNOME-Mini.iso
    Извлечены файлы vmlinuz и initrd.img:
    mount -o loop /var/lib/tftpboot/almalinux-9.4/isolinux/AlmaLinux-9.4-x86_64-Live-GNOME-Mini.iso /mnt
    cp /mnt/isolinux/vmlinuz /var/lib/tftpboot/almalinux-9.4/isolinux/
    cp /mnt/isolinux/initrd.img /var/lib/tftpboot/almalinux-9.4/isolinux/
    umount /mnt
    Теперь PXE сервер настроен для загрузки Ubuntu 24.04.1 и AlmaLinux 9.4 с соответствующими файлами и конфигурацией.

--- pxe_install.sh
+++ pxe_install.sh
@@ -15,8 +15,7 @@ sudo systemctl enable --now nfs-server
 cat <<EOF | sudo tee /etc/dnsmasq.conf > /dev/null
 # Настройки для PXE сервера
 interface=eth0           # Используем интерфейс eth0, замените на нужный
-dhcp-boot=pxelinux.0,192.168.2.244  # Имя файла загрузчика и IP PXE-сервера(если DHCP сервером является др сервер или роутер)
-#dhcp-range=192.168.2.20,192.168.2.50,12h # Диапазон IP для DHCP если он развернут на этом сервере
+dhcp-range=192.168.1.20,192.168.1.50,12h # Диапазон IP для DHCP
 dhcp-boot=pxelinux.0     # Имя файла загрузчика
 enable-tftp              # Включаем TFTP
 tftp-root=/var/lib/tftpboot # Папка с TFTP файлами
@@ -29,27 +28,49 @@ sudo systemctl restart dnsmasq
 sudo mkdir -p /var/lib/tftpboot
 cd /var/lib/tftpboot
 
-# Копирование файлов загрузчика (syslinux)
-# Скачиваем pxelinux.0 и другие необходимые файлы
-cp /path/to/syslinux-6.03/bios/com32/pxelinux/pxelinux.0 /var/lib/tftpboot/
-cp /path/to/syslinux-6.03/bios/com32/menu/ldlinux.c32 /var/lib/tftpboot/
-cp /path/to/syslinux-6.03/bios/com32/menu/menu.c32 /var/lib/tftpboot/
+# Копирование файлов загрузчика
+wget https://mirrors.edge.kernel.org/pub/linux/utils/boot/syslinux/syslinux-6.04-pre1.tar.gz
+tar -xzf syslinux-6.04-pre1.tar.gz
+
+# Копирование pxelinux.0
+cp syslinux-6.04-pre1/bios/core/pxelinux.0 /var/lib/tftpboot/
 
 # Создание конфигурации PXE
 mkdir -p /var/lib/tftpboot/pxelinux.cfg
 cat <<EOF | sudo tee /var/lib/tftpboot/pxelinux.cfg/default > /dev/null
-DEFAULT ubuntu
-LABEL ubuntu
-    KERNEL vmlinuz
-    APPEND initrd=initrd.img
-    MENU LABEL Install Ubuntu
-    IPAPPEND 2
+DEFAULT menu
+LABEL menu
+    MENU TITLE PXE Boot Menu
+    MENU BACKGROUND splash.png
+    TIMEOUT 100
+    ONTIMEOUT local
+    PROMPT 0
+    LABEL ubuntu
+        MENU LABEL Ubuntu 24.04.1 LTS
+        KERNEL /ubuntu-installer/amd64/linux
+        APPEND initrd=/ubuntu-installer/amd64/initrd.gz
+    LABEL almalinux
+        MENU LABEL AlmaLinux 9.4
+        KERNEL /almalinux-9.4/isolinux/vmlinuz
+        APPEND initrd=/almalinux-9.4/isolinux/initrd.img
 EOF
 
-# Скачиваем необходимые файлы ядра и initrd для Ubuntu
+# Скачиваем необходимые файлы ядра и initrd для Ubuntu 24.04.1
 cd /var/lib/tftpboot
-wget http://archive.ubuntu.com/ubuntu/dists/focal/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/linux -O vmlinuz
-wget http://archive.ubuntu.com/ubuntu/dists/focal/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/initrd.gz -O initrd.img
+mkdir -p ubuntu-installer/amd64
+wget https://releases.ubuntu.com/24.04.1/ubuntu-24.04.1-desktop-amd64.iso -O /var/lib/tftpboot/ubuntu-installer/amd64/ubuntu-24.04.1-desktop-amd64.iso
+mount -o loop /var/lib/tftpboot/ubuntu-installer/amd64/ubuntu-24.04.1-desktop-amd64.iso /mnt
+cp /mnt/casper/vmlinuz /var/lib/tftpboot/ubuntu-installer/amd64/
+cp /mnt/casper/initrd /var/lib/tftpboot/ubuntu-installer/amd64/
+umount /mnt
+
+# Скачиваем файлы для AlmaLinux 9.4
+mkdir -p almalinux-9.4/isolinux
+wget https://raw.repo.almalinux.org/almalinux/9.4/live/x86_64/AlmaLinux-9.4-x86_64-Live-GNOME-Mini.iso -O /var/lib/tftpboot/almalinux-9.4/isolinux/AlmaLinux-9.4-x86_64-Live-GNOME-Mini.iso
+mount -o loop /var/lib/tftpboot/almalinux-9.4/isolinux/AlmaLinux-9.4-x86_64-Live-GNOME-Mini.iso /mnt
+cp /mnt/isolinux/vmlinuz /var/lib/tftpboot/almalinux-9.4/isolinux/
+cp /mnt/isolinux/initrd.img /var/lib/tftpboot/almalinux-9.4/isolinux/
+umount /mnt
 
 # Настройка NFS для раздачи файлов
 echo "/var/lib/tftpboot *(ro,sync,no_root_squash)" | sudo tee -a /etc/exports
@@ -64,51 +85,4 @@ sudo firewall-cmd --permanent --zone=public --add-service=tftp
 sudo firewall-cmd --permanent --zone=public --add-service=nfs
 sudo firewall-cmd --reload
 
-echo "PXE сервер установлен и настроен. Перезагрузите сервер."
-
-#!/bin/bash
-# Обновление системы
-sudo dnf update -y
-# Установка необходимых пакетов
-sudo dnf install -y dnsmasq tftp-server nfs-utils syslinux wget tmux curl tar
-# Включение и запуск необходимых сервисов
-sudo systemctl enable --now dnsmasq
-sudo systemctl enable --now tftp.socket
-sudo systemctl enable --now nfs-server
-# Настройка dnsmasq для PXE, без раздачи IP
-cat <<EOF | sudo tee /etc/dnsmasq.conf > /dev/null
-# Настройки для PXE сервера
-interface=eth0                  # Используем интерфейс eth0, замените на нужный
-dhcp-boot=pxelinux.0,192.168.2.244  # Имя файла загрузчика и IP PXE-сервера
-enable-tftp                     # Включаем TFTP
-tftp-root=/var/lib/tftpboot     # Папка с TFTP файлами
-EOF
-# Перезапуск dnsmasq с новыми настройками
-sudo systemctl restart dnsmasq
-# Настройка TFTP сервера
-sudo mkdir -p /var/lib/tftpboot
-cd /var/lib/tftpboot
-# Копирование файлов загрузчика
-wget https://github.com/ricksumner/syslinux/releases/download/v6.04/pxelinux.0 -O /var/lib/tftpboot/pxelinux.0
-# Создание конфигурации PXE
-mkdir -p /var/lib/tftpboot/pxelinux.cfg
-cat <<EOF | sudo tee /var/lib/tftpboot/pxelinux.cfg/default > /dev/null
-DEFAULT linux
-LABEL linux
-    KERNEL vmlinuz
-    APPEND initrd=initrd.img
-EOF
-# Скачивание необходимых файлов ядра и initrd для AlmaLinux
-cd /var/lib/tftpboot
-wget http://archive.ubuntu.com/ubuntu/dists/focal-updates/main/installer-amd64/current/legacy-images/netboot/ubuntu-installer/amd64/linux -O vmlinuz
-wget http://archive.ubuntu.com/ubuntu/dists/focal-updates/main/installer-amd64/current/legacy-images/netboot/ubuntu-installer/amd64/initrd.gz -O initrd.img
-# Настройка NFS для раздачи файлов
-echo "/var/lib/tftpboot *(ro,sync,no_root_squash)" | sudo tee -a /etc/exports
-# Перезапуск NFS
-sudo exportfs -r
-sudo systemctl restart nfs-server
-# Открытие портов на firewall
-sudo firewall-cmd --permanent --zone=public --add-service=tftp
-sudo firewall-cmd --permanent --zone=public --add-service=nfs
-sudo firewall-cmd --reload
 echo "PXE сервер установлен и настроен. Перезагрузите сервер."
